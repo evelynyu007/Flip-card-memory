c6917ad7b45321736855ac0771e18f7513a06592
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-21 17:45:43 -0400

    html css skeleton and images for background and the game

---
+++ app.js
@@ -0,0 +1,9 @@
+/*-------------------------------- Constants --------------------------------*/
+// Define the required constants:
+
+/*---------------------------- Variables (state) ----------------------------*/
+// 1) Define the required variables used to track the state of the game
+// 1.1) Use an array to represent
+// 1.3) use a move to represent one hit
+// the move is correct if two
+// the move is wrong
