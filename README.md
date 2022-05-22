ec96ef12962c177030145a9db86d55afa2c34369
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-22 11:20:05 -0400

    add pseudo code and update img names

--- README.md
+++ README.md
@@ -1,40 +1,37 @@
 # 🧐 Flip-card-memory 🧐
 
 # User Story
+
 As a user I want to
-* See a login page and input user name and user password
-* Choose a mode - easy or hard
-* Easy has 4 by 4 grid and I need to find 2 same images
-* Hard has 6 by 6 grid and I need to find 3 same images
-* Be able to login to start the game
-* The game will show all the images in 1 second 
-* The initial score is 0
-* Start 1 minute timer in a descedning bar or counter
-* Flip two/three same cards at the same time, score + 1
-* Flip two/three different cards at the same time, score -1
-* Find all the same cards, I will hear hooray sound effect
-* Fail to find all the same cards in one mintuer, I will hear Oh No, Oh No, Oh No No No (https://www.youtube.com/watch?v=_t9y1_WRneY)
-* Get my score and ranking at the end of game
-* I can restart a new game 
-* I will have a different pattern of images for a new game
-* I can enable/disable background music/sound effects by a button
-* I can play it on phone
 
-
+- See a login page and input user name and user password
+- Choose a mode - easy or hard
+- Easy has 3 by 4 grid and I need to find 2 same images
+- Hard has 4 by 6 grid and I need to find 3 same images
+- Be able to login to start the game
+- The game will show all the images in 3 seconds (Easy) and 2 seconds(Hard)
+- The initial score is 0
+- Start 1 minute timer in a descedning bar or counter
+- Flip two/three same cards at the same time, score + 1
+- Flip two/three different cards at the same time, score -1
+- Find all the same cards, I will hear hooray sound effect
+- Fail to find all the same cards in one mintuer, I will hear Oh No, Oh No, Oh No No No (https://www.youtube.com/watch?v=_t9y1_WRneY)
+- Get my score and ranking at the end of game
+- I can restart a new game
+- I will have a different pattern of images for a new game
+- I can enable/disable background music/sound effects by a button
+- I can play it on phone
 
 Technologies Used
-* HTML
-* CSS
-* JavaScript (logic and DOM manipulation)
 
+- HTML
+- CSS
+- JavaScript (logic and DOM manipulation)
 
-# Wireframes: 
+# Wireframes:
 
 ![image](https://user-images.githubusercontent.com/19142112/169660967-13afec5c-9912-4901-bc12-763e83cc97e4.png)
 
-
 ![image](https://user-images.githubusercontent.com/19142112/169660977-abe163c8-48ca-47d2-9f27-dd5ffd4c357c.png)
 
-
 ![image](https://user-images.githubusercontent.com/19142112/169660983-1adeda7a-781e-4ccd-9a25-126b1763b820.png)
-
--- app.js
+++ app.js
@@ -1,9 +1,65 @@
-/*-------------------------------- Constants --------------------------------*/
-// Define the required constants:
+/*-----------------------------------------------------------------------------*/
+/*-------------------------------- Login Part ---------------------------------*/
+/*-----------------------------------------------------------------------------*/
+/** Constants & Variables */
+//LATER: password has to have at least one number, one letter
+let playerName, password, forgotPswd;
+let gameMode = "easy";
+let gameOn = false;
+///LOGIN eventListener then
+gameOn = true;
 
-/*---------------------------- Variables (state) ----------------------------*/
-// 1) Define the required variables used to track the state of the game
-// 1.1) Use an array to represent
-// 1.3) use a move to represent one hit
-// the move is correct if two
-// the move is wrong
+/*-----------------------------------------------------------------------------*/
+/*-------------------------------- Game Part ----------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+/** Constants & Variables */
+// 1.1) an array to represent all the cards
+let arrCards = [];
+// 1.2) an array to represent selected two cards
+let selectedTwoCards = [];
+// 1.2) an array to represent selected Correct two cards
+let selectedAllCards = [];
+// 2) score to represent current score
+let score = 0;
+// 3) 60 seconds timer
+let timer = 60;
+/** Event Listeners */
+const gamePage = document.querySelector("#game-page");
+
+/** add background image (later: randomlize it) */
+
+/** Add cards and data class id into Grid */
+
+/** Randomize Cards Pattern */
+// arrCards.sort by Math.random * 12 +1
+
+/** Show all the cards for 3s then flip to "cardCover"*/
+
+/** Flip two cards */
+//push the two cards into selectedTwoCards
+
+/** Verify if two cards are same */
+// check by their class names
+
+/** if correct .. */
+// push correct two cards into selectedAllCards
+// score+1 otherwise score-1
+
+/** 60s timer */
+// -1s every second after game begins
+if (timer === 0) {
+  gameOn = false;
+}
+
+/** found all the corrected pair of cards */
+// check selectedAllCards.length = ;
+
+/** restart the game */
+// call randomize cards function, showCards1s function...
+// eventLisetenser ... gameOn = true;
+// array.length = 0;
+
+/*------------------------------------------------------------------------------*/
+/*-------------------------------- Ranking Part (optional)----------------------*/
+/*------------------------------------------------------------------------------*/
